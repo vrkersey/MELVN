ebde8d033ea61b09e7dddb6cb7a5c57857530920
Victor Kersey <vrkersey@gmail.com>
2017-09-21 19:26:45 -0600

    Implemented Dead Zone

--- Downhill Roller/Assets/_scripts/playerController.cs
+++ Downhill Roller/Assets/_scripts/playerController.cs
@@ -1,20 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour {
 
     private playerController ball;
-    
+    private string currentScene;
 
 	// Use this for initialization
 	void Start () {
         ball = this;
-        Debug.Log(transform.parent);
+        currentScene = SceneManager.GetActiveScene().name;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void OnTriggerEnter(Collider c)
+    {
+        if (c.CompareTag("Death_Zone"))
+        {
+            SceneManager.LoadScene(currentScene);
+        }
+    }
 }
