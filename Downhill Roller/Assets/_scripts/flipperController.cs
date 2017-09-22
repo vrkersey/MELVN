f01524fcd8e355e0a94256476fda13954649dfab
Victor Kersey <vrkersey@gmail.com>
2017-09-21 19:08:49 -0600

    got camera to follow ball

---
+++ Downhill Roller/Assets/_scripts/flipperController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipperController : MonoBehaviour {
+
+    private flipperController thisFlipper;
+    private int dir;
+    private bool action = false;
+    private float angle = 0f;
+
+    public float maxOffset = 45f;
+    public float speed = 200f;
+
+    // Use this for initialization
+    void Start () {
+        thisFlipper = this;
+        dir = thisFlipper.CompareTag("Flipper_CW") ? 1 : -1;
+
+        Debug.Log("Game has Started");        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        //Debug.Log(angle);
+        if (Input.anyKey)
+        {
+            action = true;
+        }
+
+        if (action)
+        {
+            
+            if (angle <= maxOffset)
+            {
+                float delta = speed * Time.deltaTime * dir;
+                transform.Rotate(Vector3.up, delta);
+                angle += Math.Abs(delta);
+            }
+            else
+            {
+                action = false;
+            }
+        }
+        else
+        {
+            if (angle > 0)
+            {
+                float delta = speed * Time.deltaTime * -dir;
+                transform.Rotate(Vector3.up, delta);
+                angle -= Math.Abs(delta);
+            }
+        }
+        
+    }
+}
---
+++ Downhill Roller/Assets/_scripts/playerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private playerController ball;
+    
+
+	// Use this for initialization
+	void Start () {
+        ball = this;
+        Debug.Log(transform.parent);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
