2282f7408a2ef622f2bd653f48da338bb83cb54c
StiggyGame <32176329+StiggyGame@users.noreply.github.com>
2017-09-28 12:26:28 -0600

    Add files via upload

---
+++ Downhill Roller/Assets/_scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform Player;
+	public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Player.position + offset;
+		
+	}
+}
