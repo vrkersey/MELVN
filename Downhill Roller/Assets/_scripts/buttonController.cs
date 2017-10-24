1721e456093669ba0578332313bada835cf12a18
Victor Kersey <vrkersey@gmail.com>
2017-10-23 21:03:39 -0600

    button controller

--- Downhill Roller/Assets/_scripts/buttonController.cs
+++ Downhill Roller/Assets/_scripts/buttonController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class buttonController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		//load availible levels to dropdown
 	}
 	
 	// Update is called once per frame
@@ -14,7 +16,16 @@ public class buttonController : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.Escape))
         {
             print("exit pressed");
-            Application.Quit();
         }
 	}
+
+    public void Exit_Level()
+    {
+
+    }
+
+    public void Start_Level(Dropdown levelDropdown)
+    {
+        print(levelDropdown.value);
+    }
 }
