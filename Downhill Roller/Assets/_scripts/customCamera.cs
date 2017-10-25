2fd7dfc11ca20e405788415b911dac900bcf7062
Victor Kersey <vrkersey@gmail.com>
2017-10-24 21:40:23 -0600

    Fixed camera issue and added HUD to Eds level

--- Downhill Roller/Assets/_scripts/customCamera.cs
+++ Downhill Roller/Assets/_scripts/customCamera.cs
@@ -9,32 +9,29 @@ public class customCamera : MonoBehaviour {
 
     public Transform Player;
 	public float zoomSpeed;
+    public float distanceFromBall = 10f;
 
     void Start()
     {
-        offset = new Vector3(0, 0, -10f);
+        offset = new Vector3(0, 0, distanceFromBall);
+        distanceFromBall = -distanceFromBall;
     }
 
 	// Update is called once per frame
 	void Update ()
     {
+        float mag = Player.GetComponent<Rigidbody>().velocity.magnitude;
 
+        transform.position = new Vector3(Player.position.x, Player.position.y, transform.position.z);
+        offset.z = distanceFromBall - mag;
+        Vector3 zoom = Player.position + offset;
+        Vector3 newPos = Vector3.SmoothDamp(transform.position, zoom, ref m_CurrentVelocity, 2.5f);
+        newPos.z = newPos.z < distanceFromBall ? newPos.z : distanceFromBall;
+        transform.position = newPos;
         if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0)) && locked)
         {
             locked = false;
-            transform.position = new Vector3(Player.position.x, Player.position.y, -10f);
-            Debug.Log(transform.position);
+            transform.position = Player.position + offset;
         }
-
-        float mag = Player.GetComponent<Rigidbody>().velocity.magnitude;
-
-        transform.position =
-        offset.z = -10f - mag;
-        offset.z = offset.z < -10f ? offset.z : -10f;
-        Vector3 zoom = Player.position + offset;
-        print(zoom);
-        Vector3 newPos = Vector3.SmoothDamp(transform.position, zoom, ref m_CurrentVelocity, 2.5f);
-        transform.position = newPos;
-        
-   	}
+    }
 }
