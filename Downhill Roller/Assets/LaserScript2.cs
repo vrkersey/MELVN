0c279f951cb628145d1266e7dd259d4c8c08fd56
Nick <nickday06@gmail.com>
2017-12-04 15:14:58 -0700

    Made new laser beam particle effect and Prefabed the new laser.

---
+++ Downhill Roller/Assets/LaserScript2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript2 : MonoBehaviour {
+    public Transform startPoint;
+    public Transform endPoint;
+    LineRenderer laserLine;
+	// Use this for initialization
+	void Start () {
+        laserLine = GetComponent<LineRenderer>();
+        laserLine.startWidth = 0.2f;
+        laserLine.endWidth = 0.2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        laserLine.SetPosition (0, startPoint.position);
+        laserLine.SetPosition(1, endPoint.position );
+	}
+}
